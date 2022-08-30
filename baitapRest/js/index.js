b085af5109268c18e5d4ea42e32bb3d7138a2f36
HuynhNam <ctnam789@gmail.com>
2022-08-30 21:08:50 +0700

    3

--- baitapRest/js/index.js
+++ baitapRest/js/index.js
@@ -2,10 +2,10 @@
 let CalAvr = (...arrDiem) => {
     let number=0
     for (let i=0; i < arrDiem.length; i++){
-        number += arrDiem[i]
+        number += arrDiem[i];
 
-        return number/(arrDiem.length)
-    }
+        // return number/(arrDiem.length)
+    }return number/(arrDiem.length)
 }
 
 document.querySelector("#btnKhoi1").onclick = () => {
@@ -15,5 +15,5 @@ document.querySelector("#btnKhoi1").onclick = () => {
 
 document.querySelector("#btnKhoi2").onclick = () => {
     let Diem =document.querySelectorAll("#khoi2 input");
-    document.querySelector("#tbKhoi2").innerHTML=CalAvr(+Diem[0].value,+Diem[1].value,+Diem[2].value);
+    document.querySelector("#tbKhoi2").innerHTML=CalAvr(+Diem[0].value,+Diem[1].value,+Diem[2].value,+Diem[3].value);
 }
